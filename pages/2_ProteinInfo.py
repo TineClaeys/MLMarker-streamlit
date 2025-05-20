e01bcf5d5fdaa43fa0a1dc1223bc61998f37a95f
Tine claeys <58697771+TineClaeys@users.noreply.github.com>
2025-05-20 10:04:36 +0200

    improved page navigation

--- pages/2_ProteinInfo.py
+++ pages/2_ProteinInfo.py
@@ -42,7 +42,7 @@ abundance_filter = st.selectbox("Select abundance filter", options=["All", "Pres
 shap_filter = st.selectbox("Select model impact", options=["All", "Positive", "Negative"])
 # letsgo = st.button("Let's go!")
 # Function to display proteins with pagination
-def display_paginated_proteins(protein_df, selected_tissue, abundance_df, shap_df, page_size=15):
+def display_paginated_proteins_slider(protein_df, selected_tissue, abundance_df, shap_df, page_size=15):
     # Filter protein data based on the selected tissue
     selected_proteins = set(shap_df.index).intersection(set(abundance_df['id'].values.tolist()))
     st.session_state["selected_proteins"] = selected_proteins
@@ -54,7 +54,7 @@ def display_paginated_proteins(protein_df, selected_tissue, abundance_df, shap_d
     # Calculate the total number of pages
     total_proteins = len(subset_proteins)
     total_pages = (total_proteins // page_size) + (1 if total_proteins % page_size != 0 else 0)
-
+    
     # Select the current page number using a slider
     current_page = st.slider(
         "Select Page",
@@ -81,6 +81,49 @@ def display_paginated_proteins(protein_df, selected_tissue, abundance_df, shap_d
     # Display the dataframe with clickable UniProt links
     st.write(f"Protein level values for {selected_tissue}, Page {current_page} of {total_pages}")
     st.markdown(page_df[['Protein', 'Entry', 'MLMarker value', 'Abundance', 'UniProt Description', 'UniProt Tissue specificity']].to_html(escape=False, index=False), unsafe_allow_html=True)
+
+def display_paginated_proteins(protein_df, selected_tissue, abundance_df, shap_df, page_size=15):
+    # Filter protein data based on the selected tissue
+    selected_proteins = set(shap_df.index).intersection(set(abundance_df['id'].values.tolist()))
+    st.session_state["selected_proteins"] = selected_proteins
+    subset_proteins = protein_df[protein_df['id'].isin(selected_proteins)]
+    subset_proteins['MLMarker value'] = subset_proteins['id'].map(shap_df)
+    subset_proteins['Abundance'] = subset_proteins['id'].map(abundance_df.set_index('id')['Abundance'])
+    subset_proteins = subset_proteins.sort_values(by='MLMarker value', ascending=False)   
+
+    # Calculate the total number of pages
+    total_proteins = len(subset_proteins)
+    total_pages = (total_proteins // page_size) + (1 if total_proteins % page_size != 0 else 0)
+    if "current_page" not in st.session_state:
+        st.session_state.current_page = 1
+    options = ['◀', '▶']
+
+    selection = st.pills("Navigate pages", options, selection_mode = "single")
+
+    if selection == "◀" and st.session_state.current_page > 1:
+        st.session_state.current_page -= 1
+    elif selection == "▶" and st.session_state.current_page < total_pages:
+        st.session_state.current_page += 1
+
+    current_page = st.session_state.current_page
+    # Calculate the start and end indices for the current page
+    start_idx = (current_page - 1) * page_size
+    end_idx = min(current_page * page_size, total_proteins)
+
+    # Slice the DataFrame to get the relevant page
+    page_df = subset_proteins.iloc[start_idx:end_idx]
+
+    # Add UniProt link dynamically using Protein ID
+    page_df['Protein'] = page_df['id'].apply(lambda x: f'<a href="https://www.uniprot.org/uniprotkb/{x}/entry" target="_blank">{x}</a>')
+
+    # Drop unnecessary columns and rename others
+    page_df.drop(columns=['Unnamed: 0', 'id'], inplace=True)
+    page_df.rename(columns={'entry name': 'Entry', 'protein_names':'UniProt Description', 'tissue_specificity': 'UniProt Tissue specificity'}, inplace=True)
+    page_df['UniProt Tissue specificity'].fillna('No tissue specificity information available in UniProt', inplace=True)
+    # Display the dataframe with clickable UniProt links
+    st.write(f"Protein level values for {selected_tissue}, Page {current_page} of {total_pages}")
+    st.markdown(page_df[['Protein', 'Entry', 'MLMarker value', 'Abundance', 'UniProt Description', 'UniProt Tissue specificity']].to_html(escape=False, index=False), unsafe_allow_html=True)
+
 
 if "show_proteins" not in st.session_state:
     st.session_state.show_proteins = False
